bb8f760f04cc2af26ea1c19de836ec45ed4443bd
iZezito <emersonsilva81240@gmail.com>
2023-03-15 13:41:49 -0300

    inserção do tema dark

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {useEffect, useState} from "react";
 import Nav from "react-bootstrap/Nav";
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Button from 'react-bootstrap/Button'
 import Modal from "react-bootstrap/Modal";
 import "./App.css";
 import NoticiaStore from "./store/Store";
@@ -21,55 +19,56 @@ const App = () => {
     }
 
 
+    return (
+        <div className="grid-container bg-dark">
+            <div className="container-nav">
+                <Nav defaultActiveKey="/home" className="flex-column">
+                    <Nav.Link href="/home">Active</Nav.Link>
+                    <Nav.Link eventKey="link-1">Link</Nav.Link>
+                    <Nav.Link eventKey="link-2">Link</Nav.Link>
+                </Nav>
 
-  return (
-    <div className="grid-container">
-      <div className="container-nav">
-        <Nav defaultActiveKey="/home" className="flex-column">
-          <Nav.Link href="/home">Active</Nav.Link>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
-        </Nav>
+            </div>
+            <div className="container justify-content-center" style={{width: '100vh', height: '100vh'}}>
+                {store.noticias.map((item) => {
+                    return (
+                        <Card style={{width: '50rem'}} key={item.id} className={'justify-content-center mt-3 mb-5 bg-dark'}>
+                            <Card.Body>
+                                <Card.Title className={'text-center text-light'}>{item.titulo}</Card.Title>
+                                <Card.Text className={'text-light'}>
+                                    {item.descricao}
+                                </Card.Text>
+                                <Card.Subtitle className={'text-light'}
+                                    onClick={() => handleShow(item.comentarios)}>{item.comentarios.length} comentários</Card.Subtitle>
+                                <input type={'text'} className={'form-text text-light bg-dark'}
+                                       onChange={(e) => store.setComentario(e.target.value)}/>
 
-      </div>
-      <div className="container justify-content-center" style={{width:'100vh', height:'100vh'}}>
-          {store.noticias.map((item) => {
-            return (
-                <Card style={{ width: '50rem' }} key={item.id} className={'justify-content-center mt-3 mb-5'}>
-                    <Card.Body>
-                        <Card.Title>{item.titulo}</Card.Title>
-                        <Card.Text>
-                            {item.descricao}
-                        </Card.Text>
-                        <Card.Subtitle onClick={() => handleShow(item.comentarios)}>{item.comentarios.length} comentários</Card.Subtitle>
-                        <input type={'text'} className={'form-text'} onChange={(e) => store.setComentario(e.target.value)}/>
-                        <button className={'btn btn-primary'} onClick={() => store.enviarComentario(item.id)}><MdSend/></button>
-                    </Card.Body>
-                </Card>
-            )})
-          }
-          <Modal show={show} onHide={handleClose}>
-              <Modal.Header closeButton>
-                  <Modal.Title>Comentários</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                  {comentarios.map((comentario) =>{
-                      return (
-                          <ListGroup>
-                              <ListGroup.Item>{comentario.texto}</ListGroup.Item>
-                          </ListGroup>
-                      )
-                  })}
-              </Modal.Body>
-          </Modal>
+                                <MdSend onClick={() => store.enviarComentario(item.id)}
+                                        style={{borderRadius: 20}} size={25} color={'white'} className={'ms-2 mb-1'}/>
 
-      </div>
-    </div>
-    
-  );
-};
+                            </Card.Body>
+                        </Card>
+                    )
+                })
+                }
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title className={'text-center'}>Comentários</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {comentarios.map((comentario) => {
+                            return (
+                                <p>{comentario.texto}</p>
+                            )
+                        })}
+                    </Modal.Body>
+                </Modal>
 
-export default App;
+            </div>
+        </div>
+
+    );
+}
+    ;
+
+    export default App;
